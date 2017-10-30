beb9bc208ca760c2322d25fe27dea9d96b181bbe
nafnaf <nafpigos@gmail.com>
2017-10-30 16:11:18 +0200

    forLoopRetTabulate7

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -29,7 +29,7 @@ def scrap():
     div = page_soup.find_all("strong")   
     for value in div:
         item = value.text.strip()
-    return item
+        return item
 #tab
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
