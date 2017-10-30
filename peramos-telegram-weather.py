25e5aade1d9c380229fc83a6201169d43212311a
nafnaf <nafpigos@gmail.com>
2017-10-30 16:22:39 +0200

    101s

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -37,7 +37,7 @@ def handle(msg):
     print(msg, content_type, chat_type, chat_id, strftime("%a, %d %b %Y %H:%M:%S +0000"))
 
     if content_type == 'text':
-        bot.sendMessage(chat_id, div)
+        bot.sendMessage(chat_id, scrap())
 
 
 bot = telepot.Bot(TOKEN)
