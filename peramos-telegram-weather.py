af630f29e0c16850d32fde13c81b5df747dbf82e
nafnaf <nafpigos@gmail.com>
2017-10-31 01:33:11 +0200

    new4

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -29,7 +29,7 @@ def scrap():
     div = page_soup.find_all("strong")   
     for item in div:
         item = item.text.strip()
-    return tabulate(item)    #print(item)
+        return item    #print(item)
 #tab
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
