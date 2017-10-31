6708af10f4b4e9ce1230c29de726f303a6336a66
nafnaf <nafpigos@gmail.com>
2017-10-31 18:10:29 +0200

    recurse

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -27,7 +27,7 @@ def scrap():
    # [page_soup.find_all("strong")[1].text.strip()+" "+page_soup.find_all("strong")[2].text.strip()]]
     #return tabulate(values_list)
     for tag in page_soup.find_all("tr")[2]:
-        c = tag.text
+        c = tag
     return c
 #tab
 def handle(msg):
