a5841b7cca4f2410181722b3699164ee2594db83
nafnaf <nafpigos@gmail.com>
2017-11-02 21:51:04 +0200

    recurse

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -25,8 +25,8 @@ def scrap():
     page_soup = soup(page, "html.parser")
     x = page_soup.findAll("tr")[4:10]
     for item in x[5:]:
-        return item.text.strip()
-
+        k = item.text.strip()
+    return k
    # values_list = [
    # [page_soup.find_all("strong")[1].text.strip()+" "+page_soup.find_all("strong")[2].text.strip()]]
     #return tabulate(values_list)
