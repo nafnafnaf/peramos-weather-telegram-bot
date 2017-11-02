3e35ad307531d9a2871c2cd6bd43df8e62042e8a
nafnaf <nafpigos@gmail.com>
2017-11-02 15:26:53 +0200

    recurse

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -33,7 +33,7 @@ def scrap():
      #   return(tag.text)
     #x = page_soup("td")[10].text.strip()
     #y = str(x)
-    for td in page_soup.find("td").text.strip():
+    for td in page_soup.find_all("td"):
         return str(td)
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
