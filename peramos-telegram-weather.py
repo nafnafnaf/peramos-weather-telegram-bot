88717f9a29c94abf6112f9a53ea88b698994ff33
nafnaf <nafpigos@gmail.com>
2017-11-02 19:27:50 +0200

    recurse

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -17,12 +17,12 @@ TOKEN= os.environ['TOKEN']
 #updater.bot.set_webhook("https://meteokavgr.herokuapp.com/" + TOKEN)
 #updater.idle()
 
-#def scrap():
-url = "http://penteli.meteo.gr/stations/neaperamos/"
-req = urlopen(url)
-page = req.read()
-req.close()
-page_soup = soup(page, "html.parser")
+def scrap():
+    url = "http://penteli.meteo.gr/stations/neaperamos/"
+    req = urlopen(url)
+    page = req.read()
+    req.close()
+    page_soup = soup(page, "html.parser")
    # values_list = [
    # [page_soup.find_all("strong")[1].text.strip()+" "+page_soup.find_all("strong")[2].text.strip()]]
     #return tabulate(values_list)
@@ -33,9 +33,9 @@ page_soup = soup(page, "html.parser")
      #   return(tag.text)
     #x = page_soup("td")[10].text.strip()
     #y = str(x)
-for tr in page_soup.find_all("tr"):
-    x = tr.text
-y = x
+    for tr in page_soup.find_all("tr"):
+        return tr.text
+
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(msg, content_type, chat_type, chat_id, strftime("%a, %d %b %Y %H:%M:%S +0000"))
