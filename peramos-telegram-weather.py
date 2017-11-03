25b0c2cd5c1617fe605249773d67c27f3cb88b58
nafnaf <nafpigos@gmail.com>
2017-11-03 12:52:59 +0200

    try

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -23,7 +23,7 @@ def scrap():
     page = req.read()
     req.close()
     page_soup = soup(page, "html.parser")
-    x = page_soup.findAll("tr")[0:10]
+    x = page_soup.findAll("tr")[5:10]
     for item in x:
         return item.text.strip()
    # L=[page_soup.findAll("tr")]
