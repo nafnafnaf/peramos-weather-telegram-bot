804a0998084fdd0fef984c740c5e0bb99f4b6769
nafnaf <nafpigos@gmail.com>
2017-11-03 15:15:22 +0200

    new

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -33,7 +33,7 @@ def scrap():
      #   return(tag.text)
     #x = page_soup("td")[10].text.strip()
     #y = str(x)
-   for i in len(page_soup.find_all("td")):
+    for i in len(page_soup.find_all("td")):
         return str(page_soup("td")[i])
 
     #for item in x:
