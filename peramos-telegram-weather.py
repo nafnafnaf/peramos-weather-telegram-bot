a20c386fbf9a74d990bb87cdaa244da7d40cfe32
nafnafnaf <nafpigos@gmail.com>
2017-11-08 11:10:21 +0200

    tabulateX

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -37,6 +37,7 @@ def scrap():
     for tr in page_soup.find_all("tr")[5:32]:
         x = tr.text.strip()
         table = tabulate(x)
+    table = scrap
 #    return x
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
