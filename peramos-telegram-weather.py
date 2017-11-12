68622f4ffae86e2a624b508e893779da1b7d5a5d
nafnafnaf <nafpigos@gmail.com>
2017-11-13 01:04:42 +0200

    sliced

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -26,7 +26,7 @@ def scrape():
     page_soup = soup(page, "html.parser")
     for td in page_soup.find_all("td"):
         for tr in td.page_soup.find_all("tr"):
-            return str(tr.text)
+            print(str(tr.text))
 
         
 def handle(msg):
