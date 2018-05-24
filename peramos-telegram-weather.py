910307459e9694d241fbaaa18bb7153b8b684a77
nafnafnaf <nafpigos@gmail.com>
2018-05-24 11:57:34 +0300

    utfno

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -31,7 +31,7 @@ def handle(msg):
     print(msg, content_type, chat_type, chat_id, strftime("%a, %d %b %Y %H:%M:%S +0000"))
 
     if content_type == 'text':
-        bot.sendMessage(chat_id, x)
+        bot.sendMessage(chat_id, 'jijijij')
 
 bot = telepot.Bot(TOKEN)
 #some_api = some_api_lib.connect(some_api_token)
