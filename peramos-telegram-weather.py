031db3031453671e9cd472522a96e7f864498fec
nafnafnaf <nafpigos@gmail.com>
2018-05-24 13:31:30 +0300

    utfno

--- peramos-telegram-weather.py
+++ peramos-telegram-weather.py
@@ -24,7 +24,7 @@ def scrape():
     req.close()
     page_soup = soup(page, "html.parser")
     #x = "hithere"
-    x = page_soup.find_all("span", {"lang":"el"},"strong")
+    x = page_soup.find_all(["span", {"lang":"el"},"strong"])
     return str(x)
         
 def handle(msg):
